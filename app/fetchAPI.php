98ef5f76b399a394307faa6c2cc334e3f90ec7bf
DeventerEvert <107989093+DeventerEvert@users.noreply.github.com>
2024-05-23 13:14:42 +0200

    More API changes

--- app/fetchAPI.php
+++ app/fetchAPI.php
@@ -6,18 +6,58 @@ header('Content-Type: application/json');
 
 parse_str(file_get_contents("php://input"), $_PUT);
 
-$userId = isset($_GET['user_id']) ? (int)$_GET['user_id'] : null;
-$swipedUserId = isset($_GET['swiped_user_id']) ? (int)$_GET['swiped_user_id'] : null;
+$userId = isset($_GET['user_id']) ? (int) $_GET['user_id'] : null;
+$swipedUserId = isset($_GET['swiped_user_id']) ? (int) $_GET['swiped_user_id'] : null;
 $type = isset($_GET['type']) ? $_GET['type'] : null;
 
-if (is_null($userId) || is_null($swipedUserId) || is_null($type)) {
-    http_response_code(400);
-    echo json_encode(['message' => 'Missing required parameters']);
-    exit();
-}
-$swipeService = new API();
-$response = $swipeService->registerSwipe($userId, $swipedUserId, $type);
 
-http_response_code($response['status']);
-echo json_encode(['message' => $response['message']]);
+$swipeService = new API();
+if ($_SERVER["REQUEST_METHOD"] === "GET") 
+{
+    if (!is_null($userId) && !is_null($swipedUserId) && !is_null($type)) {
+    $response = $swipeService->registerSwipe($userId, $swipedUserId, $type);
+    http_response_code($response['status']);
+    echo json_encode(['message' => $response['message']]);
+    }
+}
+
+$singleImage = isset($_GET['singleImage']) ? (int) $_GET['singleImage'] : null;
+$allImage = isset($_GET['allImage']);
+$singleLooking_for = isset($_GET['singleLooking_for']) ? (int) $_GET['singleLooking_for'] : null;
+$allLooking_for = isset($_GET['allLooking_for']);
+$singleMatch = isset($_GET['singleMatch']) ? (int) $_GET['singleMatch'] : null;
+$allMatch = isset($_GET['allMatch']);
+$singleMessage = isset($_GET['singleMessage']) ? (int) $_GET['singleMessage'] : null;
+$allMessage = isset($_GET['allMessage']);
+$singleProfile = isset($_GET['singleProfile']) ? (int) $_GET['singleProfile'] : null;
+$allProfile = isset($_GET['allProfile']);
+$singleReport = isset($_GET['singleReport']) ? (int) $_GET['singleReport'] : null;
+$allReport = isset($_GET['allReport']);
+$singleSexual_preference = isset($_GET['singleSexual_preference']) ? (int) $_GET['singleSexual_preference'] : null;
+$allSexual_preference = isset($_GET['allSexual_preference']);
+$singleSpecial_field = isset($_GET['singleSpecial_field']) ? (int) $_GET['singleSpecial_field'] : null;
+$allSpecial_field = isset($_GET['allSpecial_field']);
+$singleSwipe = isset($_GET['singleSwipe']) ? (int) $_GET['singleSwipe'] : null;
+$allSwipe = isset($_GET['allSwipe']);
+$singleUser = isset($_GET['singleUser']) ? (int) $_GET['singleUser'] : null;
+$allUser = isset($_GET['allUser']) ? (int) $_GET['allUser'] : null;
+$allMessageByUser = isset($_GET['allMessageByUser']) ? (int) $_GET['allMessageByUser'] : null;
+$allImageByUser = isset($_GET['allImageByUser']) ? (int) $_GET['allImageByUser'] : null;
+
+
+
+if (!is_null($singleImage) || !is_null($allImage) || !is_null($singleLooking_for) || !is_null($allLooking_for) || !is_null($singleMatch) || !is_null($allMatch) || !is_null($singleMessage) || !is_null($allMessage) ||
+    !is_null($singleProfile) || !is_null($allProfile) || !is_null($singleReport) || !is_null($allReport) || !is_null($singleSexual_preference) || !is_null($allSexual_preference) || !is_null($singleSpecial_field) ||
+    !is_null($allSpecial_field) || !is_null($singleSwipe) || !is_null($singleUser) || !is_null($allUser) || !is_null($allImageByUser) || !is_null($allMessageByUser)) 
+{
+$result = $swipeService->retrieveFromDatabase($singleImage, $allImage, $singleLooking_for, $allLooking_for, $singleMatch, $allMatch, $singleMessage, $allMessage, $singleProfile, $allProfile, $singleReport, $allReport, 
+$singleSexual_preference, $allSexual_preference, $singleSpecial_field, $allSpecial_field, $singleSwipe, $allSwipe, $singleUser, $allUser, $allImageByUser, $allMessageByUser);
+http_response_code(200); 
+echo json_encode($result);
+exit();
+}
+
+//Voor chats en images, fijn vinden als alle chats en images van 1 userID opgehaald worden
+//alle chats en images ophalen via sender id
+
 ?>
--- handlers/retrieveAPI.php
+++ handlers/retrieveAPI.php
@@ -47,7 +47,6 @@ class API {
                 $stmt->bindParam(1, $userId, PDO::PARAM_INT);
                 $stmt->bindParam(2, $swipedUserId, PDO::PARAM_INT);
                 $stmt->execute();
-
                 $this->conn->commit();
                 // Als er een match is gevonden
                 return ['status' => 202, 'message' => 'Match gevonden!'];
@@ -62,5 +61,186 @@ class API {
             return ['status' => 500, 'message' => 'Swipe niet goed aangemaakt: ' . $e->getMessage()];
         }
     }
+
+    public function retrieveFromDatabase($singleImage, $allImage, $singleLooking_for, $allLooking_for, $singleMatch, $allMatch, $singleMessage, $allMessage, $singleProfile, $allProfile, $singleReport, $allReport, 
+    $singleSexual_preference, $allSexual_preference, $singleSpecial_field, $allSpecial_field, $singleSwipe, $allSwipe, $singleUser, $allUser, $allImageByUser, $allMessageByUser)
+    {
+        $result = [];
+        
+        if($_SERVER["REQUEST_METHOD"] === "GET")
+        {
+            
+
+            if($singleImage != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM image WHERE image_id = ?');
+                $stmt->bindParam(1, $singleImage, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleImage'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allImage != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM image');
+                $stmt->execute();
+                $result['allImage'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleLooking_for != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM looking_for WHERE profile_id = ?');
+                $stmt->bindParam(1, $singleLooking_for, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleLooking_for'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allLooking_for != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM looking_for');
+                $stmt->execute();
+                $result['allLooking_for'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleMatch != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM `match` WHERE match_id = ?');
+                $stmt->bindParam(1, $singleMatch, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleMatch'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allMatch != null)
+            {
+                $stmt = $this->conn->prepare("SELECT * FROM `match`");
+                $stmt->execute();
+                $result['allMatch'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleMessage != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM "message" WHERE message_id = ?');
+                $stmt->bindParam(1, $singleMessage, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleMessage'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allMessage != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM "message"');
+                $stmt->execute();
+                $result['allMessage'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleProfile != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM "profile" WHERE user_id = ?');
+                $stmt->bindParam(1, $singleProfile, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleProfile'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allProfile != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM "profile"');
+                $stmt->execute();
+                $result['allProfile'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleReport != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM report WHERE report_id = ?');
+                $stmt->bindParam(1, $singleReport, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleReport'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allReport != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM report');
+                $stmt->execute();
+                $result['allReport'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleSexual_preference != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM sexual_preference WHERE profile_id = ?');
+                $stmt->bindParam(1, $singleSexual_preference, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleSexual_preference'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allSexual_preference != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM sexual_preference');
+                $stmt->execute();
+                $result['allSexual_preference'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleSpecial_field != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM special_field WHERE field_id = ?');
+                $stmt->bindParam(1, $singleSpecial_field, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleSpecial_field'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allSpecial_field != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM special_field');
+                $stmt->execute();
+                $result['allSpecial_field'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleSwipe != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM swipe WHERE swipe_swiper_user_id = ?');
+                $stmt->bindParam(1, $singleSwipe, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleSwipe'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allSwipe != null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM swipe');
+                $stmt->execute();
+                $result['allSwipe'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($singleUser !== null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM user WHERE user_id = ?');
+                $stmt->bindParam(1, $singleUser, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['singleUser'] = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if($allUser !== null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM user');
+                $stmt->execute();
+                $result['allUser'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($allImageByUser !== null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM image WHERE Profile_User_idUser = ?');
+                $stmt->bindParam(1, $allImageByUser, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['allImageByUser'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            if($allMessageByUser !== null)
+            {
+                $stmt = $this->conn->prepare('SELECT * FROM message WHERE message_id = ?');
+                $stmt->bindParam(1, $allMessageByUser, PDO::PARAM_INT);
+                $stmt->execute();
+                $result['allMessageByUser'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+                return $result;
+    }
+}
+
+
 }
 ?>
