eb8fee3a3c032cddb8309f47eca11673dd0e2077
DeventerEvert <107989093+DeventerEvert@users.noreply.github.com>
2024-06-03 10:48:40 +0200

    Added assets
    Great

---
+++ assets/img/favicons/loader.php
@@ -0,0 +1,6 @@
+<link rel="apple-touch-icon" href="assets/img/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicons/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="512x512" href="assets/img/favicons/android-chrome-512x512.png">
